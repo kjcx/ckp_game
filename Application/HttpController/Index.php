09bad6b382531b2308ae7e9551317c7a87d2c729
科技创新2013 <1339796113@qq.com>
2018-04-02 16:22:06 +0800

    aaa

---
+++ Application/HttpController/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dongkai
+ * Date: 2018/4/2
+ * Time: 下午4:18
+ */
+namespace App\HttpController;
+
+class Index extends Controller
+{
+    function index(){
+        var_dump(1);
+    }
+}
