824658ea3a5db51bcb162fee283ed6dc951fee78
杜鹏 <277578870@qq.com>
2018-04-13 14:08:26 +0800

    合并

--- Application/Event/MainEventHelper.php
+++ Application/Event/MainEventHelper.php
@@ -5,6 +5,8 @@
  * Date: 2018/4/3
  * Time: 下午5:38
  * 注册一些主要的事件 当服务启动的时候
+ * 带有helper的类 属于静态事件调用  我瞎起的名字  就是说直接调用静态方法
+ * 当然还有动态调用的方法
  */
 
 namespace App\Event;
@@ -18,30 +20,6 @@ use EasySwoole\Core\Swoole\ServerManager;
 
 class MainEventHelper
 {
-    /**
-     *当websocket建立连接时候的事件
-     */
-    public static function onWebsocketConnection(Event $event)
-    {
-
-    }
-
-    /**
-     * 注册mysql服务
-     */
-    public static function registerMysqlPool()
-    {
-        $max = Config::getInstance()->getConf('MYSQL_SERVER.pool.max');
-        $min = Config::getInstance()->getConf('MYSQL_SERVER.pool.min');
-        PoolManager::getInstance()->addPool(MysqlPool::class, $min, $max);
-    }
-
-    public static function registerRedisPool()
-    {
-        $max = Config::getInstance()->getConf('REDIS_SERVER.pool.max');
-        $min = Config::getInstance()->getConf('REDIS_SERVER.pool.min');
-        PoolManager::getInstance()->addPool(RedisPool::class, $min, $max);
-    }
 
     /**
      * 热加载
