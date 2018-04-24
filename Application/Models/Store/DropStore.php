2b7b94cde5d8db120b27b8ffbb9f25212cb1faf4
杜鹏 <277578870@qq.com>
2018-04-24 19:53:52 +0800

    背包更新

--- Application/Models/Store/DropStore.php
+++ Application/Models/Store/DropStore.php
@@ -4,12 +4,14 @@
  * User: dupeng
  * Date: 2018/4/21
  * Time: 下午3:10
+ * 掉落库数据mongo直接读取比较慢 框架初始化的时候加载到redis中
  */
 
 namespace App\Models\Store;
 
 use App\Models\Model;
 use App\Traits\MongoTrait;
+use EasySwoole\Config;
 use EasySwoole\Core\Component\Di;
 
 class DropStore extends Model
@@ -17,19 +19,58 @@ class DropStore extends Model
     use MongoTrait;
 
     private $uid;
-    private $dropType = ['MenCloth','WomenCloth','BeautyShop','DepartmentStore','JewelryStore','ManDiscountShop','WomenDiscountShop'];
-    private $shopType = 'ShopCenterType';
+
+    private $mongoTable = 'ckzc.dropShop';
+    private $redisKey;
+    private $dropShopData;
 
     public function __construct(Int $uid)
     {
         parent::__construct();
         $this->uid = $uid;
+        $this->collection = $this->getMongoClient();
+        $this->init();
     }
 
-
-    private function ()
+    /**
+     * 初始化个人掉落库
+     */
+    private function init()
     {
+        $this->dropShopData = $this->mongo->ckzc->dropShop->find()->toArray();
 
     }
+
+
+
+
+
+    /**
+     *
+     */
+    public function randGoods($dropLib)
+    {
+        $randArr = [];
+        foreach ($dropLib as $k => $v) {
+
+        }
+    }
+    /**
+     * 缓存掉落库
+     */
+    public function cacheDropShop()
+    {
+        
+    }
+    /**
+     *
+     */
+    public function refreshDropShop()
+    {
+        return $this->dropShopData;
+    }
+
+
+
 
 }
